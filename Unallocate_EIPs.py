c6a054eeac2641a789055603f0552e91f9270b5b
Dushan Wijesinghe <dushan566@gmail.com>
2022-12-22 11:15:01 +0000

    Update Unallocate_EIPs.py

--- Unallocate_EIPs.py
+++ Unallocate_EIPs.py
@@ -1,10 +1,23 @@
 # This will unallocate unattached Elastic IPs
 import boto3
 
-def lambda_handler(event, context):
-    client = boto3.client('ec2')
+import boto3
+
+# def lambda_handler(event, context):
+#     client = boto3.client('ec2')
+#     addresses_dict = client.describe_addresses()
+#     for eip_dict in addresses_dict['Addresses']:
+#         if "NetworkInterfaceId" not in eip_dict:
+#             print(eip_dict['PublicIp'])
+#             client.release_address(AllocationId=eip_dict['AllocationId'])
+
+def lambda_handler():
+    client = boto3.client('ec2', region_name='eu-west-1')
     addresses_dict = client.describe_addresses()
     for eip_dict in addresses_dict['Addresses']:
         if "NetworkInterfaceId" not in eip_dict:
-            print(eip_dict['PublicIp'])
-            client.release_address(AllocationId=eip_dict['AllocationId'])
+            if "eipalloc-xxxxxxxxxxxxxx" in eip_dict.values(): #excludes a spesific EIP
+                print("excluding " + eip_dict['AllocationId'] + "IP:" + eip_dict['PublicIp'])
+            else:
+                print("Deleting " + eip_dict['AllocationId'] + "IP:" + eip_dict['PublicIp'])
+                client.release_address(AllocationId=eip_dict['AllocationId'])
